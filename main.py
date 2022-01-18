4feb2c1cd243dbaa967daa8a8cc7f32c94491f4e
bynthytn <80202809+bynthytn@users.noreply.github.com>
2022-01-18 21:30:11 +0300

    vseshtohochesh

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+path = "X:\\_cereal\\nobe"
+destination = f'{path}\\subtitles'
+files = os.listdir(path)
+
+for i in files:
+    for j in os.listdir(f'{path}\\{i}'):
+        if j.endswith('.srt'):
+            dst = f'{destination}\\{j}'
+            src = f'{path}\\{i}\\{j}'
+            os.rename(src, dst)
+
+
+
+
+
+
